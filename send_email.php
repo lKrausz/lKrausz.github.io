e237eb4534eb5cf4b0b4a297442d7251654ad966
Vi <krausz.st@gmail.com>
2024-05-12 20:56:26 +0400

    Update send_email.php

--- send_email.php
+++ send_email.php
@@ -1,20 +1,27 @@
+
 <?php
-if(!empty($_POST["send"])) {
-	$name = $_POST["name"];
-    $email = $_POST["email"];
-    $company = $_POST["company"];
-    $whatsapp = $_POST["whatsapp"];
-	
-    $toEmail = "krausz.st@gmail.com";
-  
-    $mailHeaders = "Name: " . $name .
-	"\r\n Email: ". $email  . 
-	"\r\n Company: ". $company  . 
-	"\r\n WhatsApp: " . $whatsapp . "\r\n";
+
+$name = $_POST["name"];
+$email = $_POST["email"];
+$company = $_POST["company"];
+$whatsapp = $_POST["whatsapp"];
+
+$subject = "Отправка формы - Контакт с сайта";
+
+$mailHeaders = "From: Website Contact Form <your_website_email@example.com>\r\n";
+$mailHeaders .= "Reply-To: $email\r\n"; 
+$mailHeaders .= "Content-Type: text/plain; charset=UTF-8\r\n";
 
 
-	mail($toEmail, $userName, $mailHeaders);
+$message = "Имя: $name\n";
+$message .= "Email: $email\n";
+$message .= "Компания: $company\n";
+$message .= "WhatsApp: $whatsapp\n";
 
-    echo"Message sent!"
+if (mail($toEmail, $subject, $message, $mailHeaders)) {
+  echo "Email sent successfully!";
+} else {
+  echo "Error sending email.";
 }
+
 ?>
